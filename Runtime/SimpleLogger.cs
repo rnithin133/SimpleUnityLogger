322da681b346c03099cae4f57f54e095114bca73
K R Nitheesh <rnithin133@gmail.com>
2022-05-03 18:41:27 +0530

    Version 2.0.3
    Added Api for Non-Object Class
    Added Non-Prefix Functions

--- Runtime/SimpleLogger.cs
+++ Runtime/SimpleLogger.cs
@@ -7,22 +7,45 @@ public static class SimpleLogger {
         return $"<color={color}>{myStr}</color>";
     }
 
+    [Obsolete]
     private static void DoLog(Action<string, Object> logFunction, string prefix, Object myObj, params object[] msg) {
 #if UNITY_EDITOR
-        string name = (myObj ? myObj.name : "NullObject").Color("lightblue");
+        string name = (myObj ? myObj.name : "Null").Color("lightblue");
         logFunction($"{prefix}[{name}]: {String.Join("; ", msg)}\n ", myObj);
 #endif
     }
 
-    private static void DoLog(Action<string> logFunction, string prefix, params object[] msg) {
+    private static void DoLog(Action<string, Object> logFunction, Object myObj, params object[] msg) {
 #if UNITY_EDITOR
-        // string name = (myObj ? myObj.name : "NullObject").Color("lightblue");
-        logFunction($"{prefix}: {String.Join("; ", msg)}\n ");
+        string name = (myObj ? myObj.name : "Null").Color("lightblue");
+        logFunction($"[{name}]: {String.Join("; ", msg)}\n ", myObj);
+#endif
+    }
+
+    private static void DoLog(Action<string> logFunction, params object[] msg) {
+#if UNITY_EDITOR
+        // logFunction($"[Null]{String.Join("; ", msg)}\n ");
+        logFunction("[" + "Null".Color("lightblue") + "]: " + $"{String.Join("; ", msg)}\n ");
+        // logFunction(msg + "\n ");
 #endif
     }
 
     public static void Log(params object[] msg) {
-        DoLog((s) => Debug.Log(s), "", msg);
+#if UNITY_EDITOR
+        DoLog(Debug.Log, msg);
+#endif
+    }
+
+    public static void LogWarning(params object[] msg) {
+#if UNITY_EDITOR
+        DoLog(Debug.LogWarning, msg);
+#endif
+    }
+
+    public static void LogError(params object[] msg) {
+#if UNITY_EDITOR
+        DoLog(Debug.LogError, msg);
+#endif
     }
 
     /// <summary>
@@ -31,19 +54,28 @@ public static class SimpleLogger {
     /// <param name="myObj"> object name</param>
     /// <param name="msg"> prints message</param>
     public static void Log(this Object myObj, params object[] msg) {
-        DoLog((s, o) => Debug.Log(s, o), "", myObj, msg);
+#if UNITY_EDITOR
+        DoLog(Debug.Log, myObj, msg);
+#endif
     }
 
     public static void LogError(this Object myObj, params object[] msg) {
         // DoLog(Debug.LogError, "<!>".Color("red"), myObj, msg);
-        DoLog((s, o) => Debug.LogError(s, o), "!".Color("red"), myObj, msg);
+        // DoLog((s, o) => Debug.LogError(s, o), "!".Color("red"), myObj, msg);
+#if UNITY_EDITOR
+        DoLog(Debug.LogError, myObj, msg);
+#endif
     }
 
     public static void LogWarning(this Object myObj, params object[] msg) {
-        DoLog(Debug.LogWarning, "⚠️".Color("yellow"), myObj, msg);
+#if UNITY_EDITOR
+        DoLog(Debug.LogWarning, myObj, msg);
+#endif
     }
 
     public static void LogSuccess(this Object myObj, params object[] msg) {
-        DoLog(Debug.Log, "☻".Color("green"), myObj, msg);
+#if UNITY_EDITOR
+        DoLog(Debug.Log, myObj, msg);
+#endif
     }
 }
