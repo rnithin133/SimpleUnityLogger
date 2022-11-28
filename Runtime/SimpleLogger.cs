f38b55f8f967bc431a68cb57241d85fb6c5d593a
K R Nitheesh <rnithin133@gmail.com>
2022-11-28 22:54:41 +0530

    SimpleLogger Version 2.1.0

--- Runtime/SimpleLogger.cs
+++ Runtime/SimpleLogger.cs
@@ -1,81 +1,90 @@
 ﻿using System;
 using UnityEngine;
+using LogType = KR.SimpleLogger.LogType;
 using Object = UnityEngine.Object;
 
+namespace KR.SimpleLogger {
+    [Flags]
+    public enum LogType : byte {
+        None = 0,
+        Log = 1,
+        LogWarning = 2,
+        LogError = 4,
+        LogSuccess = 8
+    }
+}
+
 public static class SimpleLogger {
-    public static string Color(this string myStr, string color) {
+    public static LogType LogType;
+
+    static SimpleLogger() {
+        Debug.Log("Simple Logger Enabled EnabledLogType = " + LogType);
+    }
+
+    public static void EnableLogger(LogType logType) {
+        LogType = logType;
+    }
+
+    private static string Color(this string myStr, string color) {
         return $"<color={color}>{myStr}</color>";
     }
 
-    [Obsolete]
-    private static void DoLog(Action<string, Object> logFunction, string prefix, Object myObj, params object[] msg) {
-#if UNITY_EDITOR
-        string name = (myObj ? myObj.name : "Null").Color("lightblue");
-        logFunction($"{prefix}[{name}]: {String.Join("; ", msg)}\n ", myObj);
-#endif
-    }
-
     private static void DoLog(Action<string, Object> logFunction, Object myObj, params object[] msg) {
-#if UNITY_EDITOR
         string name = (myObj ? myObj.name : "Null").Color("lightblue");
-        logFunction($"[{name}]: {String.Join("; ", msg)}\n ", myObj);
-#endif
+        logFunction($"[{name}]: {string.Join("; ", msg)}\n ", myObj);
     }
 
     private static void DoLog(Action<string> logFunction, params object[] msg) {
-#if UNITY_EDITOR
-        // logFunction($"[Null]{String.Join("; ", msg)}\n ");
-        logFunction("[" + "Null".Color("lightblue") + "]: " + $"{String.Join("; ", msg)}\n ");
-        // logFunction(msg + "\n ");
-#endif
+        logFunction("[" + "Null".Color("lightblue") + "]: " + $"{string.Join("; ", msg)}\n ");
     }
 
     public static void Log(params object[] msg) {
-#if UNITY_EDITOR
+        if ((LogType & LogType.Log) == 0)
+            return;
         DoLog(Debug.Log, msg);
-#endif
     }
 
     public static void LogWarning(params object[] msg) {
-#if UNITY_EDITOR
+        if ((LogType & LogType.LogWarning) == 0)
+            return;
         DoLog(Debug.LogWarning, msg);
-#endif
     }
 
     public static void LogError(params object[] msg) {
-#if UNITY_EDITOR
+        if ((LogType & LogType.LogError) == 0)
+            return;
         DoLog(Debug.LogError, msg);
-#endif
     }
 
-    /// <summary>
-    /// Debug.Log()
-    /// </summary>
+    public static void LogSuccess(params object[] msg) {
+        if ((LogType & LogType.LogSuccess) == 0)
+            return;
+        DoLog(Debug.Log, msg);
+    }
+
     /// <param name="myObj"> object name</param>
     /// <param name="msg"> prints message</param>
     public static void Log(this Object myObj, params object[] msg) {
-#if UNITY_EDITOR
+        if ((LogType & LogType.Log) == 0)
+            return;
         DoLog(Debug.Log, myObj, msg);
-#endif
-    }
-
-    public static void LogError(this Object myObj, params object[] msg) {
-        // DoLog(Debug.LogError, "<!>".Color("red"), myObj, msg);
-        // DoLog((s, o) => Debug.LogError(s, o), "!".Color("red"), myObj, msg);
-#if UNITY_EDITOR
-        DoLog(Debug.LogError, myObj, msg);
-#endif
     }
 
     public static void LogWarning(this Object myObj, params object[] msg) {
-#if UNITY_EDITOR
+        if ((LogType & LogType.LogWarning) == 0)
+            return;
         DoLog(Debug.LogWarning, myObj, msg);
-#endif
+    }
+
+    public static void LogError(this Object myObj, params object[] msg) {
+        if ((LogType & LogType.LogError) == 0)
+            return;
+        DoLog(Debug.LogError, myObj, msg);
     }
 
     public static void LogSuccess(this Object myObj, params object[] msg) {
-#if UNITY_EDITOR
+        if ((LogType & LogType.LogSuccess) == 0)
+            return;
         DoLog(Debug.Log, myObj, msg);
-#endif
     }
 }
